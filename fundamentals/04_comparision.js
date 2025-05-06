91580769a1cff05ff520221f6042922d5cf4024d
Amitanshu Varshney <amitanshu.varshney@gmail.com>
2025-05-06 23:19:28 +0530

    comparision

---
+++ fundamentals/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2===1);
+// console.log(2!==1);
+
+// console.log("2">1); 
+// // automatic conversion of string to 
+// console.log("02" > 1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0)
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+
+// == & <= function differently
+
+// === & !== are strict equality and strict inequality operators. They do not perform type coercion.
+
+console.log("2"==2); //true because of conversion.
+console.log("2"===2); //false because of no conversion and strict checking.

9c2990e5a2dba5073c043a13a517ac6ddbea4ec4
Amitanshu Varshney <amitanshu.varshney@gmail.com>
2025-05-06 21:36:43 +0530

    learnt about datatypes

---
+++ fundamentals/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version ; not necessary
+
+// alert(3+3)  we are using nodejs not browser
+
+console.log(3+3)
+console.log("hitesh")
+
+let name = "hitesh" //string => ""
+let age = 22  //number => 2 power 53
+let isLoggedIn = true //boolean => true/false
+//null => stand alone value; not a number
+//undefined => variable is not defined; not a number
+//symbol => unique; not a number
+
+
+//obect
+
+console.log(typeof undefined) ;//undefined
+console.log(typeof null) ;//object

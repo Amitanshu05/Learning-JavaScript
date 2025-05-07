9bff899f70eeb7f4def85fa7c983a33d09e6fe49
Amitanshu Varshney <amitanshu.varshney@gmail.com>
2025-05-07 20:51:25 +0530

    strings

---
+++ fundamentals/06_strings.js
@@ -0,0 +1,38 @@
+const name ="hitesh";
+const repoCount= 22
+
+// console.log(name+repoCount+"value");
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gamename= new String("hitesh-amitanshu");
+
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+
+// console.log(gamename.charAt(0));
+// console.log(gamename.indexOf("h"));
+
+const newString= gamename.substring(0,4);
+// console.log(newString);
+
+const anotherString= gamename.slice(1,4);
+// console.log(anotherString);
+
+const newstring = "    hitesh   ";
+// console.log(newstring.trim()); 
+
+const url = "https://www.google.com/";
+
+// console.log(url.replace("https://", "http://"));
+
+// console.log(url.includes("hitesh")); 
+
+// console.log(url.split("")); 
+
+const str1 = "5";
+
+console.log(str1.padStart(5, "0"));
